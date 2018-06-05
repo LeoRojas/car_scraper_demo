56eec57248aee352ac1b3d7c79a12c22b895b99c
Leonardo Rojas <lrojas@funx.io>
2018-06-04 21:26:47 -0400

    Initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,38 @@
+class HomeController < ApplicationController
+
+require 'nokogiri'
+require 'open-uri'
+
+  def index
+    # Fetch and parse HTML document
+    @car_models = get_car_models("toyota")
+  end
+
+  def get_car_models brand
+    car_url = 'https://www.toyota.com/' if brand == 'toyota'
+    car_url = 'http://www.globalsuzuki.com/automobile/' if brand == 'suzuki'
+    car_url = 'https://automobiles.honda.com/' if brand == 'honda'
+    doc = Nokogiri::HTML(open(car_url))
+    # puts "### Search for nodes by css"
+    # doc.css('nav ul.menu li a', 'article h2').each do |link|
+    #   puts link.content
+    # end
+    models = []
+      if brand == 'toyota'
+        doc.css("a[class='tcom-model-sweep-title']").each do |elem|
+          models << elem.content
+        end
+      end
+      if brand == 'suzuki'
+        doc.css("section[id='models-area'] li").each do |elem|
+          models << elem.content
+        end
+      end
+      if brand == 'honda'
+        doc.css("section[class='rzf-gry rzf-gry-carousel rzf-gry-display-x-small-true rzf-gry-display-small-true rzf-gry-display-medium-true rzf-gry-display-large-true rzf-gry-display-x-large-true'] section[class='rzf-gray info-container'] svg").each do |elem|
+          models << elem.content
+        end
+      end
+    return models
+  end
+end
